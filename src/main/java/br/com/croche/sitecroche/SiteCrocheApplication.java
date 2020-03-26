4070aadc8efd4dd91e63b083ad0a1031c14a7747
João Junior <jp.junior@live.com>
2020-03-25 23:27:15 -0300

    projeto inicial

---
+++ src/main/java/br/com/croche/sitecroche/SiteCrocheApplication.java
@@ -0,0 +1,13 @@
+package br.com.croche.sitecroche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteCrocheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteCrocheApplication.class, args);
+	}
+
+}
